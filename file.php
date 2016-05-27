78b181dc9b150d90abe3cd697a5ab6395c8bb12c
Maxime Kieffer <mk@bwets.net>
2016-05-27 11:48:15 +0200

    Plus de mitsu

--- file.php
+++ file.php
@@ -5,7 +5,6 @@
 		<?php echo 'Bonjour à tous' ?>
 		<div class="alert">Mitsubishi</div>
 		<?php
-			echo 'Mitsubishi';
 		?>
 		Une modif faite par un contributeur
 		<?php echo '<br>Nous sommes le ' . date('Y-m-d') ?>
